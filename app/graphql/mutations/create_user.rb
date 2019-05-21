92080bf806021572a4238e054307600497312bb5
Omar K <kalouo@pm.me>
2019-05-21 19:35:44 +0900

    implements createUser with GraphQL

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,20 @@
+module Mutations
+    class CreateUser < BaseMutation
+      class AuthProviderSignupData < Types::BaseInputObject
+        argument :email, Types::AuthProviderEmailInput, required: false
+      end
+  
+      argument :username, String, required: true
+      argument :auth_provider, AuthProviderSignupData, required: false
+  
+      type Types::UserType
+  
+      def resolve(username: nil, auth_provider: nil)
+        User.create!(
+          username: username,
+          email: auth_provider&.[](:email)&.[](:email),
+          password: auth_provider&.[](:email)&.[](:password)
+        )
+      end
+    end
+  end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,14 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_users, [UserType], null: false
 
+    # this method is invoked, when `all_link` fields is being resolved
+    def all_users
+      User.all
+    end
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
