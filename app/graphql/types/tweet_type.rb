c1012ca35f24ec06e669c5521b380705d109059b
Omar K <kalouo@pm.me>
2019-05-22 15:16:17 +0900

    adds createdAt to schema

--- app/graphql/types/tweet_type.rb
+++ app/graphql/types/tweet_type.rb
@@ -2,7 +2,7 @@ module Types
     class TweetType < BaseObject
       field :id, ID, null: false
       field :content, String, null: false
-      field :description, String, null: false
       field :posted_by, UserType, null: false, method: :user
+      field :created_at, String, null: false
     end
   end
