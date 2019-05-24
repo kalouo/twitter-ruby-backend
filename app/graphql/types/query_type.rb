5cc46a4a5a680e01d819a2ef0b90177af7fbc607
Omar K <kalouo@pm.me>
2019-05-24 10:13:55 +0900

    adds get_current_user query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,22 +1,21 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-    # queries are just represented as fields
-    # `all_links` is automatically camelcased to `allLinks`
     field :all_users, [UserType], null: false
 
-    # this method is invoked, when `all_link` fields is being resolved
     def all_users
       User.all
     end
 
     field :current_user_tweets, [TweetType], null: false
 
-    # this method is invoked, when `all_link` fields is being resolved
     def current_user_tweets
       Tweet.where(:user_id => context[:current_user])
     end
+
+    field :get_current_user, UserType, null: false
     
+    def get_current_user
+      context[:current_user]
+    end
   end
 end
